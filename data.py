9d2a0b81b69e6df0bf3e7a0b5a31a2ddfb18bf66
Brian Joseph <brian.joseph_2026@nyjc.edu.sg>
2026-08-21 09:37:08 +0800

    Update data.py 1.0.0

--- data.py
+++ data.py
@@ -1,1 +1,29 @@
+class Player:
+    def __init__(self):
+        self.hp = 20 # pre-set health level
+        self.attack = 2 # bare-handed punch
+        self.money = 0
+        
+    def get_hp(self):
+        pass
 
+
+    def attack(self):
+        pass
+
+    def get_inventory(self):
+        pass
+
+    def insert_inventory(self,item):
+        """ once purchased item from shop , place into inventory (max 5 items) """
+        pass
+
+    def navigation(self,direction):
+        pass
+
+
+    def get_money(self):
+        pass
+
+    def set_money(self,item):
+        pass
