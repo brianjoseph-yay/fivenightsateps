b32acc476159d88dbca640802399eb682940c929
JkAcc09 <daisykweky612@gmail.com>
2026-08-25 14:19:36 +0000

    datapy update 2.2

--- data.py
+++ data.py
@@ -42,6 +42,12 @@ class Player:
             return "Inventory full!"
         
         self.inventory.append(item)
+
+    def prompt_player_choice(self):
+        return input("> ")
+
+
+
     """
     def get_money(self):
         return self.money
@@ -94,10 +100,11 @@ class Monster(Player):
         return self.des
 
 class Room:
-    def __init__(self, contents, exits):
+    def __init__(self, contents, exits, is_final : bool):
         self.contents = contents
         self.exits = exits #dict
         self.next = {"north" : None, "east" : None, "south" : None, "west" : None}
+        self.is_final = is_final
     
     def get_exits(self):
         return self.exits
@@ -105,32 +112,39 @@ class Room:
     def get_contents(self):
         return self.contents
 
+    def is_final(self):
+        return self.is_final
+
 
 class stack: #mapping of the rooms on a level
     def __init__(self):
         self.head = None
         self.pointer = None
-        self.retreat_direction = None
+        self.retreat_path = []
 
     def push(self, room, direction):
+        if room.is_final:
+            return "final" # reached final room
+
         if direction == "north":
-            self.retreat_direction = "north"
+            self.retreat_path.append("south")
         elif direction == "south":
-            self.retreat_direction = "south"
+            self.retreat_path.append("north")
         elif direction == "east":
-            self.retreat_direction = "east"
+            self.retreat_path.append("west")
         elif direction == "west":
-            self.retreat_direction = "west"
+            self.retreat_path.append("east")
 
         if self.head is None:
             self.head = room
             self.pointer = room
-            return
+            return False
 
         current = self.pointer
         self.pointer.next[direction] = room
-        self.pointer.next[self.retreat_direction] = current
         self.pointer = room
+        self.pointer.next[self.retreat_path[-1]] = current
+        return False
 
     def forward(self, direction):
         current = self.pointer
@@ -138,24 +152,48 @@ class stack: #mapping of the rooms on a level
             self.pointer = current.next[direction]
 
             if direction == "north":
-                self.retreat_direction = "south"
+                self.retreat_path.append("south")
             elif direction == "south":
-                self.retreat_direction = "north"
+                self.retreat_path.append("north")
             elif direction == "east":
-                self.retreat_direction = "west"
+                self.retreat_path.append("west")
             elif direction == "west":
-                self.retreat_direction = "east"
+                self.retreat_path.append("east")
 
-            return
+            if self.pointer.is_final:
+                return "final" # reached final room
+
+            return False
         return "dead end"
 
     def retreat(self):
-        if self.retreat_direction is None:
+        if self.retreat_path is None or self.pointer is None:
             return "you are at the start"
-        return self.pointer.next[self.retreat_direction]
+
+        if self.pointer.is_final:
+            return "final" # reached final room
+
+        retreat_direction = self.retreat_path.pop(-1)
+        self.pointer = self.pointer.next[retreat_direction]
+
+        direction = retreat_direction
+        if direction == "north":
+            self.retreat_path.append("south")
+        elif direction == "south":
+            self.retreat_path.append("north")
+        elif direction == "east":
+            self.retreat_path.append("west")
+        elif direction == "west":
+            self.retreat_path.append("east")
+        
+        return False
 
 test = stack()
-test.push(Room(1, {"north" : None, "east" : None, "south" : None, "west" : None}), "south")
-print(test.pointer)
-test.push(Room(2, {"north" : None, "east" : None, "south" : None, "west" : None}), "north")
-print(test.pointer)
+test.push(Room(1, {"north" : None, "east" : None, "south" : None, "west" : None}, None), "south")
+print(test.pointer.contents, test.retreat_path)
+test.push(Room(2, {"north" : None, "east" : None, "south" : None, "west" : None}, None), "east")
+print(test.pointer.contents, test.retreat_path)
+test.retreat()
+print(test.pointer.contents, test.retreat_path)
+test.forward("east")
+print(test.pointer.contents, test.retreat_path)
