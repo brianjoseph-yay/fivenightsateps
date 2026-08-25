de3fca9c25ffbdef7745fef31d25a115fbe987e9
aadikainthola2026-collab <aadi.kainthola_2026@nyjc.edu.sg>
2026-08-25 15:04:11 +0800

    Update game.py version 2.0

--- game.py
+++ game.py
@@ -1,6 +1,18 @@
+import random
+
+import data
+
+
 class Game:
     def __init__(self,name):
+        self.player = None
         self.name = name
+        self.movement = []
+        self.is_gameover = False
+        
+
+    def add_player(self, player):
+        self.player = player
 
     def introduction(self):
         """ NSEW , UD """
@@ -14,46 +26,65 @@ class Game:
         print("The game will be starting shortly")
 
     
-    def attack(self):
-        pass
+    def attack(self, monster: data.monster,is_attacking : bool, item: data.Item):
+        player = self.player
+        if is_attacking == "P":
+            damage = player.attack # bare-handed attack by player
+            if item:
+                if item.name== "taser":
+                    if item.usecount > 0:
+                        monster.take_damage(item.damage) 
 
-    def win(self):
-        pass
+                elif item.name == "oil":
+                     if item.usercount > 0:
+                          player.gain_hp(item.regeneration)
+                     
+                        
+            else:
+                """ attacking monster bare-handed"""
+                monster.hp -= damage
+        if is_attacking == "M":
+            # damage done by monster is randomised
+            damage = random.randint(2,4)
+            player.hp -= damage
 
-    def lose(self):
-        pass
+
+    def get_options(self,is_attacking,room:data.room):
+        """ shows the options , Attack , healing etc"""
+        if is_attacking:
+            return ["Attack","Access Inventory","Retreat"]
+        else:
+            bin_list = []
+            for exits in room.getExits():
+                bin_list.append(exits)
+            result = bin_list + ["Attack","Access Inventory","Retreat"]
+            return result
 
 
-class Player:
-    def __init__(self):
-        self.hp= 20 # pre-set health level
-        self.attack = 2 # bare-handed punch
-        self.money = 0
+    def get_actions(self, choice):
+        player = self.player
+        if choice == "Attack":
+            return self.attack()
+        elif choice == "Inventory":
+            return player.get_inventory()
+        elif choice == "Retreat":
+            return player.map.retreat()
+        elif choice == "N":
+            return player.map.forward("north")
+        elif choice == "S":
+                    return player.map.forward("south")
+        elif choice == "E":
+                    return player.map.forward("east")
+        else:
+            return player.map.forward("west")
         
-    def get_hp(self):
-        pass
 
-
-    def attack(self):
-        pass
-
-    def get_inventory(self):
-        pass
-
-    def insert_inventory(self,item):
-        """ once purchased item from shop , place into inventory (max 5 items) """
-        pass
-
-    def navigation(self,direction):
-        pass
-
-
-    def get_money(self):
-        pass
-
-    def set_money(self,item):
-        pass
-
+    def epilogue(self):
+        """ display game over"""
+        import time
+        print(f" Awwww GAME OVER , better luck next time")
+        time.sleep(2)
+        print(f"better luck next time")
 
 
     
